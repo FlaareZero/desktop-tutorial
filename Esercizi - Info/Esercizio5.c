9bcd09687f31a46cdfa4603a8e8583f0fde41efd
AmPeRE581 <francesco.franca75@gmail.com>
2024-06-21 10:46:52 +0200

    Esercizio5 - Finito alle ore 10.47

--- Esercizi - Info/Esercizio5.c
+++ Esercizi - Info/Esercizio5.c
@@ -4,5 +4,25 @@ Tale valore deciso dall'utente deve essere maggiore di 0*/
 #include<stdio.h>
 
 int main(){
-    
+    /*Iniziallizazione Variabili*/
+    int i;
+    int n;
+    n=1;
+
+    /*Presa del parametro di conteggio*/
+    printf("Inserisci il numero da cui deve partire il conteggio:");
+    scanf("%d",&i);
+
+    /*Ciclo di conteggio*/
+
+    while (i > 0){
+        printf("%d", n);
+
+        n++;
+
+        return 0;
+    }
+
+
+
 }
