bde39b7443fc3dfc1f1c2639b2bec075632a422e
AmPeRE581 <francesco.franca75@gmail.com>
2024-06-21 11:56:40 +0200

    Esercizio6 - Terminato alle 11.56

---
+++ Esercizi - Info/Esercizio6.c
@@ -0,0 +1,31 @@
+/*Creare un applicazione che mostri la tabellina di un numero scelto dall'utente, dallo 0 fino al 10*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int numero_tabellina;
+
+    int i,n;
+
+    printf("Inserisci il numero di cui devo riportare la tabellina: ");
+    scanf("%d",&numero_tabellina);
+
+    if(i<10){
+        printf("La tabellina richiesta nn è disponibile...sorry!!!!");
+
+        return 0;
+    }
+
+    while (n<10){
+        printf("Ti mostrerò la tabellina del numero che mi hai fornito...");
+
+        printf("%d",numero_tabellina*i);
+
+        break;
+    }
+
+
+
+
+}
