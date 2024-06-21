d5363254236f840dc282c58f2cbf91ee45f95924
AmPeRE581 <francesco.franca75@gmail.com>
2024-06-21 11:03:18 +0200

    Update Esercizio5.c

--- Esercizi - Info/Esercizio5.c
+++ Esercizi - Info/Esercizio5.c
@@ -1,6 +1,8 @@
 /*Si vuole realizzare una applicazione che permetta di mostrare a schermo i numeri tipici di un conto alla rovescia partendo da un valore deciso dall'utente.
 Tale valore deciso dall'utente deve essere maggiore di 0*/
 
+
+#include<stdio.h>
 int main(){
     /*Iniziallizazione Variabili*/
         int i;
