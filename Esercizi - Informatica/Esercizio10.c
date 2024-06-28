5e9096cb78530ec69e88ff797914f38b60996396
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2024-06-28 15:51:00 +0200

    Sess pazz estrem uanm

---
+++ Esercizi - Informatica/Esercizio10.c
@@ -0,0 +1,11 @@
+/*
+Esercizio: Ricerca di un elemento in un array
+Scrivere un programma C che:
+
+Dichiara un array di interi di dimensione N (definita dall'utente).
+Permette all'utente di inserire i valori per gli elementi dell'array.
+Richiede all'utente di inserire un valore da cercare nell'array.
+Utilizza un ciclo for per cercare il valore desiderato all'interno dell'array.
+Stampa un messaggio se il valore viene trovato, indicando la sua posizione nell'array.
+Stampa un messaggio se il valore non viene trovato.
+*/
