96be82915376b926494c28bb36a87fdaf92774e6
AmPeRE581 <francesco.franca75@gmail.com>
2024-06-29 18:06:01 +0200

    -- Esercizio13 da completare, media dei voti.

---
+++ Esercizi - Informatica/Esercizio13.c
@@ -0,0 +1,27 @@
+/*Esercizio: Calcolo della media dei voti
+Scrivi un programma in C che permette all'utente di inserire un numero di voti scolastici e calcola la media dei voti inseriti. 
+Il programma deve svolgere le seguenti operazioni:
+
+- Chiedere all'utente quanti voti desidera inserire.
+- Utilizzare un ciclo per leggere i voti uno per uno.
+- Calcolare la somma dei voti.
+- Calcolare e stampare la media dei voti.
+ 
+-- Requisiti
+- Utilizzare un array per memorizzare i voti.
+- Gestire correttamente l'input dell'utente, assicurandosi che il numero di voti sia un numero positivo.
+- La media deve essere un numero in virgola mobile (float o double).*/
+
+#include<stdio.h>
+
+int main(){
+    //------------------------//
+    //***Dichiarazione Variabili Intere richieste dalla traccia***//
+    int numeri_scolastici_media;
+    //------------------------//
+    
+    printf("Quanti voti vuoi inserire? -->");
+    scanf("%d", &numeri_scolastici_media);
+
+
+}
