d9fe6e072751a64e327b84a5460638bcdf64b648
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2024-07-02 15:47:59 +0200

    Create Esame_16_Giutno.c

---
+++ Esercizi C - Traccie Esami/Esame_16_Giutno.c
@@ -0,0 +1,5 @@
+/*Nel contesto di una gara di tuffi, ogni atleta esegue un tuffo per il quale 5 giudici esprimono una votazione attribuendo un punteggio nell'intervallo dei numeri reali tra 0 e 10. Il punteggio più alto e più basso sono scartati, mentre gli altri sono sommati ed il risultato contribuisce alla valutazione definitiva dell'atleta. Ad esempio, se l'atleta n.300 riceve dai giudici i seguenti punteggi: 8, 7, 9.5, 8, 6 allora la sua valutazione sarà 26 (poichè 6 e 9,5 sono scartati dalla somma).
+
+Scrivere un programma che legge n sequenze. Ciascuna sequenza (sestina) e' costituita dall'identificativo dell'atleta (valore intero) e dai cinque punteggi attribuiti dai giudici al tuffo che ha eseguito. Il programma legge n, le n sestine di numeri e:
+
+1. Costruisce una matrice risultati nella quale sono riportati, per ogni atleta, il suo identificativo, la sua valutazione (cioè la somma dei tre punteggi validi), il punteggio più alto e quello più basso (scartati).*/
